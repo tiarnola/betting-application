5954e35ef75734715354fd4d0d108fda9768fe6d
Tiarnan Guinee <tiarnanguinee@tiarnans-mbp-2.home>
2024-07-26 12:05:10 +0100

    use hash router

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,16 @@
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { HashRouter, Route, Routes } from "react-router-dom";
 import { HomePage, EventPage } from "./pages";
 
 const App = () => {
   return (
-    <BrowserRouter>
+    <HashRouter>
       <div>
         <Routes>
-          <Route path="" element={<HomePage />} />
+          <Route path="/" element={<HomePage />} />
           <Route path="event/:eventId" element={<EventPage />} />
         </Routes>
       </div>
-    </BrowserRouter>
+    </HashRouter>
   );
 };
 
